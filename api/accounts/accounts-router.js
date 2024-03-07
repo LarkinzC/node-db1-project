0a2252f8ffb7a54dce4643a76d8906ce3ded301e
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-07 13:20:38 -0800

    asdfsdaf

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -17,11 +17,11 @@ router.get('/:id', md.checkAccountId, async (req, res, next) => {
 
 router.post('/', 
 md.checkAccountPayload, 
-md.checkAccountNameUnique, 
-(req, res, next) => {
-  // DO YOUR MAGIC
+md.checkAccountNameUnique,
+async (req, res, next) => {
   try{
-
+    const newAcc = await Account.create(req.body)
+    res.status(201).json(newAcc)
   } catch(err) {
     next(err)
   }
